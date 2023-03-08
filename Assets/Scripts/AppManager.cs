67900e100aa92a7b4c39b1cf51704c03994eec88
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-03-08 22:08:10 +0800

    feat: add wakelock (prevent screen turning off)

--- Assets/Scripts/AppManager.cs
+++ Assets/Scripts/AppManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -6,6 +7,13 @@ using UnityEngine.XR.ARFoundation;
 
 public class AppManager : MonoBehaviour
 {
+    private void Start()
+    {
+        // Disable screen lock
+        // ref: https://docs.unity3d.com/ScriptReference/Screen-sleepTimeout.html
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+
     public void QuitApp()
     {
         Application.Quit();
