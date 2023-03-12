c349e881ba42730e55d873f606becd630dd7e64a
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-03-13 03:24:30 +0800

    fix: Fixed minimap rotation

--- Assets/Scripts/PlayerFollower.cs
+++ Assets/Scripts/PlayerFollower.cs
@@ -1,17 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerFollower : MonoBehaviour
 {
     [SerializeField] private GameObject arCamera;
+    [SerializeField] private GameObject playerIndicator;
+    [SerializeField] private GameObject topDownCamera;
 
     // Update is called once per frame
     void Update()
     {
-        // player will follow ar camera
-        transform.position = arCamera.transform.position;
-
-        // minimap camera follower will handle by Position Constraint in TopDownCamera
+        var arCameraPosition = arCamera.transform.position;
+        
+        // player indicator will follow ar camera position
+        playerIndicator.transform.position = arCameraPosition;
+        
+        // top down camera will follow AR rotation in y and the player position
+        var currentTopDownCameraHeight = topDownCamera.transform.position.y;
+        topDownCamera.transform.rotation = Quaternion.Euler(90, arCamera.transform.rotation.eulerAngles.y, 0);
+        topDownCamera.transform.position = new Vector3(arCameraPosition.x, currentTopDownCameraHeight, arCameraPosition.z);
     }
 }
